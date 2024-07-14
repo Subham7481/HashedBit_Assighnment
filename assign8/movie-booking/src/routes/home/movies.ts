965eba9ba007b0c8bbc6d241392ca0afa38a0b0e
Subham <patelsk7481@gmail.com>
2024-07-14 17:59:53 +0530

    Initial commit with assignments

---
+++ assign8/movie-booking/src/routes/home/movies.ts
@@ -0,0 +1,56 @@
+import { Movie } from "../../store/movieSlice";
+
+let movies: Movie[] = [
+    {
+        name: "Hera Pheri",
+        description: "Comedy",
+        ticketPrice: 200,
+        hallNumber: 4
+    },
+    {
+        name: "Dilwale",
+        description: "Romantic",
+        ticketPrice: 150,
+        hallNumber: 2
+    },
+    {
+        name: "3 Idiots",
+        description: "Comedy",
+        ticketPrice: 300,
+        hallNumber: 1
+    },
+    {
+        name: "Dangal",
+        description: "Sports/Drama",
+        ticketPrice: 200,
+        hallNumber: 2
+    },
+    {
+        name: "Lagaan",
+        description: "Sports/Drama",
+        ticketPrice: 200,
+        hallNumber: 1
+    },
+    {
+        name: "Gully Boy",
+        description: "Romance/ Musical Drama",
+        ticketPrice: 150,
+        hallNumber: 2
+    },
+    {
+        name: "Andhadhun",
+        description: "Comedy/Thriller",
+        ticketPrice: 250,
+        hallNumber: 3
+    },
+      {
+        name: "Sholay",
+        description: "Action/Crime",
+        ticketPrice: 200,
+        hallNumber: 3
+    },
+]
+
+export {
+    movies
+}
